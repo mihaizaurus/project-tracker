10e7c3eabcb5dd90fde4a7db36cc4d8451608fba
Mihai <mihaizaurus@protonmail.com>
2025-04-05 00:14:04 +0200

    prepared groundwork for later backend work

---
+++ crates/backend/src/main.rs
@@ -0,0 +1,9 @@
+// use project_tracker_core::{models::{milestone, person, project, tag, task}, HasId};
+// use person::Person;
+// use project::Project;
+// use tag::Tag;
+// use task::Task;
+
+fn main() {
+    
+}
