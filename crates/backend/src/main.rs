5ad8b92fc5befd87c4ede81ff0956c02eff7bb47
mihaizaurus <73397939+mihaizaurus@users.noreply.github.com>
2025-04-15 02:34:56 +0200

    updated sample run

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -4,13 +4,18 @@
 // use tag::Tag;
 // use task::Task;
 
-use project_tracker_core::{builders::task_builder::TaskBuilder, factories::{project_factory::*, task_factory::*}};
-use chrono::{DateTime, Utc};
+use project_tracker_core::factories::{project_factory::*, task_factory::*, tag_factory::*};
 
 fn main() {
     let task1 = sample_task();
     println!("{task1}");
 
-    let task2 = TaskBuilder::new().with_name("This is a sample Task").with_description("Sample Description").with_due_date(Utc::now()).build();
+    let task2 = sample_task_with_due_date();
     println!("{task2}");
+
+    let project1 = sample_project_with_tags();
+    println!("{project1}");
+
+    let tag1 = sample_tag();
+    println!("{tag1}");
 }
