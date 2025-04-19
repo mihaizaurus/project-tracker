7dc063be45c1506336a767e97788165f994c7d02
Mihai <73397939+mihaizaurus@users.noreply.github.com>
2025-04-19 12:33:26 +0200

    updated project factory for DTO and built basic route for test

--- crates/backend/src/services/project_service.rs
+++ crates/backend/src/services/project_service.rs
@@ -1,14 +1,14 @@
 use crate::dto::project_dto::ProjectDTO;
-use project_tracker_core::factories::project_factory::{self, sample_project};
+use project_tracker_core::factories::project_factory::*;
 
 pub fn get_all_projects() -> Vec<ProjectDTO> {
     // TODO
     vec![
-        sample_project_for_dto(),
-        sample_project_for_dto(),
-        sample_project_for_dto(),
-        sample_project_for_dto(),
-        sample_project_for_dto(),
-        sample_project_for_dto(),
+        ProjectDTO::from(sample_project_for_dto()),
+        ProjectDTO::from(sample_project_for_dto()),
+        ProjectDTO::from(sample_project_for_dto()),
+        ProjectDTO::from(sample_project_for_dto()),
+        ProjectDTO::from(sample_project_for_dto()),
+        ProjectDTO::from(sample_project_for_dto()),
     ]
 }
